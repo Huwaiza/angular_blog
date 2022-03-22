a8be1c23d4ff330304f394ffc0966d2bced2d2b8
Huwaiza <muhammadhuwaizatahir@gmail.com>
2022-03-22 23:10:09 +0500

    interpolation cleared

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'M Huwaiza Tahir';
+  message = "My message to world is, I won't quit"
+  
+  testimonials(){
+    return "Testimonials will be provided upon demand"
+  }
+
+  obj = {
+    'job_title': "Senior Python Developer",
+    'learn': "I want to learn Angular"
+  }
+
+  arr = ['harvey spector', 'donna', 'mike ross', 'luis litt', 'danial hardmen', 'jessica pearson', 'rachel zane', 'travis scot']
+  siteUrl = window.location.href
+
 }
